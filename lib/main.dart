7e22b8c8bf6af4c371b420081ef881e22ce70a78
Threshika <threshikavijay@gmail.com>
2025-09-04 23:42:34 +0530

    Stateful  widget creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,73 @@
 import 'package:flutter/material.dart';
 
 void main(){
-runApp(MainApp());
+runApp(secondApp());
 }
 
-class MainApp extends StatelessWidget{
+// class MainApp extends StatelessWidget{
+
+//   String displayName = "Hello ";
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Change Hello to bye"),
+//           centerTitle: false,
+//         ),
+//         body: Column(
+//           children: [
+//             Text(displayName),
+//             MaterialButton(
+//               child: Text("Click"),
+//               color: Colors.lightBlue,
+//               padding: EdgeInsets.all(10),
+              
+//              onPressed: () {
+//               displayName="Bye";
+//              } ,
+//             )
+//           ],
+//         )
+//       ),
+//     );
+//   }
+// }
+
+class secondApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return secondAppState();
+  }
+}
+
+class secondAppState extends State<secondApp>{
+   String displayName = "Hello ";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text("Hello World"),
+        appBar: AppBar(
+          title: Text("Change Hello to bye"),
+          centerTitle: false,
+        ),
+        body: Column(
+          children: [
+            Text(displayName),
+            MaterialButton(
+              child: Text("Click"),
+              color: Colors.lightBlue,
+              padding: EdgeInsets.all(10),
+              
+             onPressed: () {
+              setState(() {
+                displayName="Bye";
+              });
+              
+             } ,
+            )
+          ],
+        )
       ),
     );
   }
