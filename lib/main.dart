20b1af04d19f1f45b5da5064130009052516a625
Threshika <threshikavijay@gmail.com>
2025-09-04 22:50:17 +0530

    Contact App similar to restaurant app

--- lib/main.dart
+++ lib/main.dart
@@ -1,263 +1,185 @@
 import 'package:flutter/material.dart';
 
 void main(){
- runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Restaurant Menu"),
-      backgroundColor: Colors.amber,
-      centerTitle: false,
-    ),
-    body: ListView(
-      children: [
-        Container(
-      width: double.infinity,
-      height: 85,
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            spreadRadius: 2,
-            blurRadius: 4,
-            color: Colors.black26
-          )
-        ]
-        
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Contact List"),
+        centerTitle: false,
+        backgroundColor: Colors.amber,
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
         children: [
-          Text("Chicken Rice",style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-
-          ),),
-          Text("Chicken rice with multiple falours"),
-          Text("167.9", style:TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold
-          ),)
-
-        ],
-
-      ),
-    ),
-    Container(
-      width: double.infinity,
-      height: 85,
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            spreadRadius: 2,
-            blurRadius: 4,
-            color: Colors.black26
+          Container(
+            width: double.infinity,
+            height: 70,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [BoxShadow(
+                spreadRadius: 2,
+                blurRadius: 10,
+                color: Colors.black26
+              )]
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("John Deo", style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),),
+                Text("984567898",style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal
+                ),)
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 70,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [BoxShadow(
+                spreadRadius: 2,
+                blurRadius: 10,
+                color: Colors.black26
+              )]
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("John Deo", style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),),
+                Text("984567898",style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal
+                ),)
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 70,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [BoxShadow(
+                spreadRadius: 2,
+                blurRadius: 10,
+                color: Colors.black26
+              )]
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("John Deo", style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),),
+                Text("984567898",style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal
+                ),)
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 70,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [BoxShadow(
+                spreadRadius: 2,
+                blurRadius: 10,
+                color: Colors.black26
+              )]
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("John Deo", style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),),
+                Text("984567898",style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal
+                ),)
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 70,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [BoxShadow(
+                spreadRadius: 2,
+                blurRadius: 10,
+                color: Colors.black26
+              )]
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("John Deo", style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),),
+                Text("984567898",style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal
+                ),)
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 70,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [BoxShadow(
+                spreadRadius: 2,
+                blurRadius: 10,
+                color: Colors.black26
+              )]
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("John Deo", style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),),
+                Text("984567898",style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal
+                ),)
+              ],
+            ),
           )
-        ]
-        
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("White Sauce Pasta",style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-
-          ),),
-          Text("Pasta with multiple falours"),
-          Text("249.0", style:TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold
-          ),)
-
         ],
-
       ),
-    ),
-    Container(
-      width: double.infinity,
-      height: 85,
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            spreadRadius: 2,
-            blurRadius: 4,
-            color: Colors.black26
-          )
-        ]
-        
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Chicken Briyani",style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-
-          ),),
-          Text("Chicken Briyani with multiple falours"),
-          Text("250.0", style:TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold
-          ),)
-
-        ],
-
-      ),
-    ),
-    Container(
-      width: double.infinity,
-      height: 85,
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            spreadRadius: 2,
-            blurRadius: 4,
-            color: Colors.black26
-          )
-        ]
-        
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Kothu Parotta",style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-
-          ),),
-          Text("Kothu Parotta with multiple falours"),
-          Text("80.9", style:TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold
-          ),)
-
-        ],
-
-      ),
-    ),
-    Container(
-      width: double.infinity,
-      height: 85,
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            spreadRadius: 2,
-            blurRadius: 4,
-            color: Colors.black26
-          )
-        ]
-        
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Kothu Parotta",style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-
-          ),),
-          Text("Kothu Parotta with multiple falours"),
-          Text("80.9", style:TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold
-          ),)
-
-        ],
-
-      ),
-    ),
-    Container(
-      width: double.infinity,
-      height: 85,
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            spreadRadius: 2,
-            blurRadius: 4,
-            color: Colors.black26
-          )
-        ]
-        
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Kothu Parotta",style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-
-          ),),
-          Text("Kothu Parotta with multiple falours"),
-          Text("80.9", style:TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold
-          ),)
-
-        ],
-
-      ),
-    ),
-    Container(
-      width: double.infinity,
-      height: 85,
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            spreadRadius: 2,
-            blurRadius: 4,
-            color: Colors.black26
-          )
-        ]
-        
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Kothu Parotta",style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-
-          ),),
-          Text("Kothu Parotta with multiple falours"),
-          Text("80.9", style:TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold
-          ),)
-
-        ],
-
-      ),
-    ),
-    
-      ],
     )
-  ),
- ));
+  ));
 }
