92f6db1b08996b4c79d0eb498d57235605b9ef17
Threshika <threshikavijay@gmail.com>
2025-09-05 13:58:22 +0530

    Todo App

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,100 @@
 import 'package:flutter/material.dart';
 
-void main(){
-runApp(secondApp());
+void main() {
+  runApp(MainApp());
 }
 
-
-class secondApp extends StatefulWidget{
+class MainApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return secondAppState();
+    return MainAppState();
   }
 }
 
-class secondAppState extends State<secondApp>{
-   List<String> fruits = ["Apple","Mango","Kiwi"];
+class MainAppState extends State<MainApp> {
+  String displayData = "No data found";
+  TextEditingController textEditingController = TextEditingController();
+  List<String> taskList = [];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Change Hello to bye"),
-          centerTitle: false,
+          title: Text(
+            "Todo List App",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: const Color.fromARGB(255, 44, 138, 247),
         ),
-        body: ListView.builder(
-          itemCount: fruits.length,
-          itemBuilder: (context,index){
-            return Text(fruits[index]);
-          }
-        )
+        body: Column(
+          children: [
+            Row(
+              children: [
+                //expaned for row
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: TextField(
+                      controller: textEditingController,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(10),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        label: Text("Enter a Task"),
+                        hintStyle: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ),
+                MaterialButton(
+                  color: Colors.lightBlueAccent,
+                  padding: EdgeInsets.all(10),
+                  height: 50,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(5),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      taskList.add(textEditingController.text);
+                      textEditingController.clear();
+                    });
+                  },
+                  child: Text("Add", style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+
+            //flexible for column
+            Flexible(
+              child: ListView.builder(
+                itemCount: taskList.length,
+                itemBuilder: (context, index) {
+                   return Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text(taskList[index]),
+                        ),
+                      ),
+                      MaterialButton(
+                        child: Icon(Icons.delete, color: Colors.red),
+              
+                        onPressed: () {
+                          setState(() {
+                            taskList.removeAt(index);
+                          });
+                        },
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
