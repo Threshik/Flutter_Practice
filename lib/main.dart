c0c6ee9413a78e2f10e78fe84289976e3c8ecfa3
Threshika <threshikavijay@gmail.com>
2025-09-05 20:48:45 +0530

    Calculator Application

--- lib/main.dart
+++ lib/main.dart
@@ -6,94 +6,152 @@ void main() {
 
 class MainApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return MainAppState();
-  }
+  State<MainApp> createState() => _MainAppState();
 }
 
-class MainAppState extends State<MainApp> {
-  String displayData = "No data found";
-  TextEditingController textEditingController = TextEditingController();
-  List<String> taskList = [];
+class _MainAppState extends State<MainApp> {
+  double size = 0;
+  String inputValue = "";
+  String calculatedValue="";
+  String operator ="";
 
   @override
   Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size.width / 5;
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Todo List App",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: const Color.fromARGB(255, 44, 138, 247),
-        ),
+        backgroundColor: Colors.black,
         body: Column(
           children: [
-            Row(
+            Container(
+              alignment: Alignment.bottomRight,
+              child: Text(
+                inputValue,
+                style: TextStyle(fontSize: 100, color: Colors.white),
+              ),
+            ),
+
+            Column(
               children: [
-                //expaned for row
-                Expanded(
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    child: TextField(
-                      controller: textEditingController,
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(10),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        label: Text("Enter a Task"),
-                        hintStyle: TextStyle(fontSize: 16),
-                      ),
-                    ),
-                  ),
+                Row(
+                  children: [
+                    calcButton("7", Colors.white54),
+                    calcButton("8", Colors.white54),
+                    calcButton("9", Colors.white54),
+                    calcButton("/", Colors.orange),
+                  ],
                 ),
-                MaterialButton(
-                  color: Colors.lightBlueAccent,
-                  padding: EdgeInsets.all(10),
-                  height: 50,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadiusGeometry.circular(5),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      taskList.add(textEditingController.text);
-                      textEditingController.clear();
-                    });
-                  },
-                  child: Text("Add", style: TextStyle(color: Colors.white)),
+                Row(
+                  children: [
+                    calcButton("4", Colors.white54),
+                    calcButton("5", Colors.white54),
+                    calcButton("6", Colors.white54),
+                    calcButton("*", Colors.orange),
+                  ],
+                ),
+                Row(
+                  children: [
+                    calcButton("1", Colors.white54),
+                    calcButton("2", Colors.white54),
+                    calcButton("3", Colors.white54),
+                    calcButton("-", Colors.orange),
+                  ],
+                ),
+                Row(
+                  children: [
+                    calcButton("0", Colors.white54),
+                    calcButton(".", Colors.white54),
+                    calcButton("=", Colors.orange),
+                    calcButton("+", Colors.orange),
+                  ],
                 ),
               ],
             ),
-
-            //flexible for column
-            Flexible(
-              child: ListView.builder(
-                itemCount: taskList.length,
-                itemBuilder: (context, index) {
-                   return Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          padding: EdgeInsets.all(10),
-                          child: Text(taskList[index]),
-                        ),
-                      ),
-                      MaterialButton(
-                        child: Icon(Icons.delete, color: Colors.red),
-              
-                        onPressed: () {
-                          setState(() {
-                            taskList.removeAt(index);
-                          });
-                        },
-                      ),
-                    ],
-                  );
-                },
-              ),
-            ),
+            calcButton("Clear", Colors.black),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget calcButton(String text, Color bgcolor) {
+    return InkWell(
+      onTap: () {
+        if (text == "Clear") {
+          setState(() {
+            inputValue = "";
+            calculatedValue="";
+            operator="";
+          });
+        } 
+        else if(text == "+" || text == "-" || text == "*" || text == "/"){
+          setState(() {
+            //first time operator is entered
+            if(calculatedValue.isEmpty){
+              calculatedValue=inputValue;
+            }
+            else{
+              double calc = double.parse(calculatedValue);
+              double inp = double.parse(inputValue);
+
+              if(operator=="+"){
+                calc+=inp;
+              }
+              else if(operator=="-"){
+                calc-=inp;
+              }
+              else if(operator=="*"){
+                calc*=inp;
+              }
+              else if(operator=="/"){
+                calc/=inp;
+              }
+              calculatedValue = calc.toString();
+              inputValue=""; //clr for nxt num
+              operator=text; // store new operator
+            }
+          });
+        }
+        else if(text=="="){
+          setState(() {
+           if (calculatedValue.isNotEmpty && inputValue.isNotEmpty) {
+              double calc = double.parse(calculatedValue);
+              double inp = double.parse(inputValue);
+
+              if (operator == "+") calc += inp;
+              else if (operator == "-") calc -= inp;
+              else if (operator == "*") calc *= inp;
+              else if (operator == "/") calc /= inp;
+
+              inputValue = calc.toString();
+              calculatedValue = ""; // reset after equal
+              operator = "";
+           }
+          });
+        }
+        else {
+          setState(() {
+            inputValue = inputValue + text;
+          });
+        }
+      },
+      child: Container(
+        height: size,
+        width: size,
+        margin: EdgeInsets.all(10),
+
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: bgcolor,
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
